6968061b8fb322b4730c09bc8ab43f3ba5a51f5d
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-08-03 22:26:57 +0900

    feat: ボタンON/OFFのログ出力を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
 #include <Arduino.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+// GPIO15
+const int BTN = 15;
+
+void Task1(void *pvParameters) {
+    pinMode(BTN, INPUT_PULLUP);
+    int value = 0;
+    while (1) {
+        value = digitalRead(BTN);
+        printf("%d\n", value);
+
+        // 自分のスタック使用量を確認
+        // UBaseType_t stackLeft = uxTaskGetStackHighWaterMark(NULL);
+        // printf("Stack Left(Task1) : %uword\n", stackLeft);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void setup() {
-// write your initialization code here
+    DEBUG_RP2040_PORT.begin(115200);
+    xTaskCreate(Task1, "Task1", 512, NULL, 1, NULL);
 }
 
 void loop() {
-// write your code here
+    // 何もしなくてOK
+}
+
+// スタックオーバーフロー検知
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+    // ここにエラー処理やデバッグ用の処理を書く
+    // 例：シリアル出力や無限ループで停止
+    printf("Stack overflow detected in task %s\n", pcTaskName);
+    taskDISABLE_INTERRUPTS();
+    for(;;);
 }
