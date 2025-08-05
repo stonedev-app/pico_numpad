2b3277e5d084fef1a12ffbd6ef1bacabfb857b58
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-08-05 21:11:22 +0900

    feat: ボタン押下状態の変化をログ出力できるように修正

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,53 @@
 // ボタン入力に使うピン番号(GPIO15)
 constexpr int BTN = 15;
 
+// チャタリング用待機時間(50ms)
+constexpr TickType_t DEBOUNCE_TIME = 50 / portTICK_PERIOD_MS;
+// ボタン状態監視間隔(5ms)
+constexpr TickType_t POLL_INTERVAL = 5 / portTICK_PERIOD_MS;
+
 /**
  * @brief Task1
  *
- * BTNの入力をシリアルログに出力するタスク
+ * ボタン状態をシリアルログに出力するタスク
  *
  * @param pvParameters 未使用
  */
 __attribute__((noreturn)) void Task1(void *pvParameters) {
 
-    // GPIO15をプルアップして出力に設定
+    // ボタン入力に使うピンをプルアップして出力に設定
     pinMode(BTN, INPUT_PULLUP);
 
-    // 1秒毎にGPIO15の値を読み取る
+    // 最新の安定したボタン状態
+    int lastStableBtnState = digitalRead(BTN);
+    // 最新のボタン状態
+    int lastRawBtnState = lastStableBtnState;
+    // 最新のボタン状態を更新した時間
+    TickType_t lastBtnStateChangeTime = xTaskGetTickCount();
+
+    // ボタン状態を指定時間間隔で監視
     while (true) {
-        // GPIO15の値を読み取る
-        const int value = digitalRead(BTN);
-        // シリアルログに値を出力
-        printf("%d\n", value);
 
-        // タスクのスタック使用量を確認
-        // UBaseType_t stackLeft = uxTaskGetStackHighWaterMark(nullptr);
-        // printf("Stack Left(Task1) : %uword\n", stackLeft);
+        // 現在のボタン状態を取得する
+        const int currentRawBtnState = digitalRead(BTN);
+        // 現在のボタン状態と最新のボタン状態が異なる場合
+        if (currentRawBtnState != lastRawBtnState) {
+            // ボタン状態が変わったので時間をリセット
+            lastBtnStateChangeTime = xTaskGetTickCount();
+            // 最新のボタン状態を更新
+            lastRawBtnState = currentRawBtnState;
+        }
 
-        // 1000ms待機
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // ボタン状態が変わって、かつ一定時間経過した場合
+        if (currentRawBtnState != lastStableBtnState &&
+            xTaskGetTickCount() - lastBtnStateChangeTime > DEBOUNCE_TIME) {
+            // 最新の安定したボタン状態を更新
+            lastStableBtnState = currentRawBtnState;
+            printf(lastStableBtnState == LOW ? "Pressed\n" : "Released\n");
+        }
+
+        // 指定時間待機
+        vTaskDelay(POLL_INTERVAL);
     }
 }
 
